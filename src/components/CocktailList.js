d0fba26933f2e17b14dee880788be53f4edf03b6
dr-veronica <veronica.deronzi@gmail.com>
2022-08-05 17:14:54 +0100

    new images added

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -25,32 +25,34 @@ const CocktailList = () => {
   }, [spirit])
 
   return (
-    <Container as="main">
-      <h1 className='text-center mb-4'>Drinks</h1>
-      <Row> 
-        { drinksList ? 
-          <>
-            { drinksList.drinks.map(item => {
-              const { idDrink, strDrink, strDrinkThumb } = item
-              console.log(item)
-              return (
-                <Col key={idDrink} md="6" lg="4" className='mb-4'>
-                  <Link key={idDrink} to={`/cocktail/${spirit}/${strDrink}`}>
-                    <Card>
-                      <p> { strDrink } </p>
-                      <img src={strDrinkThumb} />
-                    </Card>
-                  </Link>
-                </Col>
-              )
-            }) }
-          </> 
-          :
-          <h4 className="text-center">
-            { errors ? 'Something went wrong. Check page details' : 'Loading...'}
-          </h4> }
-      </Row>
-    </Container>        
+    <div className='list-wrapper'>
+      <Container className='list-container'>
+        <h1 className='text-center mb-4'>Drinks</h1>
+        <Row> 
+          { drinksList ? 
+            <>
+              { drinksList.drinks.map(item => {
+                const { idDrink, strDrink, strDrinkThumb } = item
+                console.log(item)
+                return (
+                  <Col key={idDrink} md="6" lg="4" className='mb-4'>
+                    <Link key={idDrink} to={`/cocktail/${spirit}/${strDrink}`}>
+                      <Card className='list-card'>
+                        <p className='text-center'> { strDrink } </p>
+                        <img src={strDrinkThumb} />
+                      </Card>
+                    </Link>
+                  </Col>
+                )
+              }) }
+            </> 
+            :
+            <h4 className="text-center">
+              { errors ? 'Something went wrong. Check page details' : 'Loading...'}
+            </h4> }
+        </Row>
+      </Container>
+    </div>        
   )
 }
 
